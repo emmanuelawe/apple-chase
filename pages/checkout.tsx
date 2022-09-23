0709585201b420e796362938e6891ef2b37c2a4e
emmanuelawe <aweemmanuelayobami@gmail.com>
2022-09-23 13:18:55 +0100

    4th commit

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -6,14 +6,13 @@ import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import Currency from "react-currency-formatter";
 import { useSelector } from "react-redux";
-// import Stripe from "stripe";
 import Button from "../components/Button";
 // import CheckoutProduct from "../components/CheckoutProduct";
 import { selectBasketItems, selectBasketTotal } from "../redux/basketSlice";
 import CheckoutProduct from '../components/CheckoutProduct';
-/* import CheckoutProduct from "../components/CheckoutProduct";
-import { fetchPostJSON } from "../utils/api-helpers";
-import getStripe from "../utils/get-stripejs"; */
+import { Stripe } from 'stripe';
+import { fetchPostJSON } from '../utils/api-helpers';
+import getStripe from "../utils/get-stripejs"; 
 
 
 const Checkout = () => {
@@ -22,6 +21,7 @@ const Checkout = () => {
     const router = useRouter()
     const [groupedItemsInBasket, setGroupedItemsInBasket] = useState(
         {} as {[key: string]: Product[]})
+    const [loading, setLoading] = useState(false)
 
 
     useEffect (() => {
@@ -32,6 +32,37 @@ const Checkout = () => {
 
         setGroupedItemsInBasket(groupedItems)
     },[items])
+
+    const createCheckoutSession = async () => {
+        setLoading(true)
+
+        const checkoutSession: Stripe.Checkout.Session = await fetchPostJSON(
+            '/api/checkout_sessions', {
+                items: items
+            }) 
+
+            // Internal Server Error
+            if ((checkoutSession as any ).statusCode === 500) {
+                console.error((checkoutSession as any).message)
+                return
+            }
+
+            // Redirect to checkout
+            const stripe = await getStripe()
+            const { error } = await stripe!.redirectToCheckout({
+                // Make the id field from the Checkout Session creation API response
+                // available to this file, so you can provide it as parameter here
+                // instead of the {{CHECKOUT_SESSION_ID}} placeholder.
+                sessionId: checkoutSession.id,
+              });
+
+             // If `redirectToCheckout` fails due to a browser or network
+            // error, display the localized error message to your customer
+            // using `error.message`.
+            console.warn(error.message);
+
+            setLoading(false)
+    }
 
   return (
     <div className='min-h-screen overflow-hidden bg-[#e7ecee]'>
@@ -93,6 +124,45 @@ const Checkout = () => {
                                 <Currency quantity={basketTotal} currency='USD'/>
                                 </h4>
                             </div>
+                        </div>
+
+                        <div className="my-14 space-y-4">
+                        <h4 className="text-xl font-semibold">
+                        How would you like to check out?
+                        </h4>
+                        <div className="flex flex-col gap-4 md:flex-row">
+                        <div className="order-2 flex flex-1 flex-col items-center rounded-xl bg-gray-200 p-8 py-12 text-center">
+                            <h4 className="mb-4 flex flex-col text-xl font-semibold">
+                         <span>Pay Monthly</span>
+                        <span>with Apple Card</span>
+                        <span>
+                        $283.16/mo. at 0% APR<sup className="-top-1">◊</sup>
+                        </span>
+                        </h4>
+                        <Button title="Check Out with Apple Card Monthly Installments" />
+                        <p className="mt-2 max-w-[240px] text-[13px]">
+                      $0.00 due today, which includes applicable full-price
+                      items, down payments, shipping, and taxes.
+                        </p>
+                        </div>
+
+                        <div className="flex flex-1 flex-col items-center space-y-8 rounded-xl bg-gray-200 p-8 py-12 md:order-2">
+                    <h4 className="mb-4 flex flex-col text-xl font-semibold">
+                      Pay in full
+                      <span>
+                        <Currency quantity={basketTotal} currency="USD" />
+                      </span>
+                    </h4>
+
+                    <Button
+                      noIcon
+                      loading={loading}
+                      title="Check Out"
+                      width="w-full"
+                      onClick={createCheckoutSession}
+                    />
+                  </div>
+                        </div>
                         </div>
                     </div>
                 </div>
